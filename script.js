6f40dfb12693c0a68a660820fc1cfd614600fc24
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-06-22 14:37:36 -0400

    animated title screen, start game button, mole death animation

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 window.onload = function () {
-    const body = document.body;
+  const body = document.body;
+
+  console.log(body);
+
+
+
+
+  const gameTitle = document.querySelector('h1');
+
+
   
-    console.log(body);
+titleGlow()
+
+  function titleGlow() {
+    gameTitle.style.transform = 'scale(1.02)';
+    gameTitle.classList.toggle('title')
+    setTimeout(titleUnglow, 1000)
+  }
+
+  function titleUnglow() {
+    gameTitle.style.transform = "";
+    gameTitle.classList.toggle('title')
+    setTimeout(titleGlow, 1000)
+  }
+
+  const startButton = document.querySelector('button');
+  const startScreen = document.querySelector('#startbox');
+
+  startButton.addEventListener("click", startGame);
+
+  function startGame () {
+    startButton.style.color = 'yellow';
+    document.body.style.backgroundImage = 'url(images/background.png)';
+    startScreen.remove();
+
+    createMole()
+  }
 
 
 
 
 
-    
 
-function createMole() {
+  function createMole() {
 
 
     let moleDiv = document.createElement('div');
@@ -22,81 +55,121 @@ function createMole() {
     //   duckDiv.classList.toggle('flap');
     // }
 
+    //Picks a random number (hole) between 1 and 9
     const randomHole = () => {
-        return Math.floor(Math.random() * 9 + 1)
-      }
-  
-   
-    //   function randomPosition(mole) {
-    //     mole.style.top = randomHeight() + 'px';
-    //     mole.style.left = randomWidth() + 'px';
-    //   }
+      return Math.floor(Math.random() * 9 + 1)
+    }
 
-    //   randomPosition(moleDiv)
-    
-    
+    //Sets a random starting position for the mole
+    function randomPosition(mole) {
+      let newHole = randomHole();
+
+      if (newHole == 1) {
+        mole.style.top = 0 + 'px';
+        mole.style.left = 395 + 'px';
+      } else if (newHole == 2) {
+        mole.style.top = 0 + 'px';
+        mole.style.left = 667 + 'px';
+      } else if (newHole == 3) {
+        mole.style.top = 0 + 'px';
+        mole.style.left = 943 + 'px';
+      } else if (newHole == 4) {
+        mole.style.top = 222 + 'px';
+        mole.style.left = 395 + 'px';
+      } else if (newHole == 5) {
+        mole.style.top = 222 + 'px';
+        mole.style.left = 667 + 'px';
+      } else if (newHole == 6) {
+        mole.style.top = 222 + 'px';
+        mole.style.left = 943 + 'px';
+      } else if (newHole == 7) {
+        mole.style.top = 444 + 'px';
+        mole.style.left = 395 + 'px';
+      } else if (newHole == 8) {
+        mole.style.top = 444 + 'px';
+        mole.style.left = 667 + 'px';
+      } else if (newHole == 9) {
+        mole.style.top = 444 + 'px';
+        mole.style.left = 943 + 'px';
+      }
+
+    }
+
+    randomPosition(moleDiv)
+
+
+    //Sets new random positions for the mole
     function moveMole(mole) {
       let newHole = randomHole();
 
-      if (newHole == 1){
+      if (newHole == 1) {
         mole.style.top = 0 + 'px';
         mole.style.left = 395 + 'px';
-      } else if (newHole == 2){
+      } else if (newHole == 2) {
         mole.style.top = 0 + 'px';
         mole.style.left = 667 + 'px';
-      } else if (newHole == 3){
+      } else if (newHole == 3) {
         mole.style.top = 0 + 'px';
         mole.style.left = 943 + 'px';
-      } else if (newHole == 4){
+      } else if (newHole == 4) {
         mole.style.top = 222 + 'px';
         mole.style.left = 395 + 'px';
-      } else if (newHole == 5){
+      } else if (newHole == 5) {
         mole.style.top = 222 + 'px';
         mole.style.left = 667 + 'px';
-      } else if (newHole == 6){
+      } else if (newHole == 6) {
         mole.style.top = 222 + 'px';
         mole.style.left = 943 + 'px';
-      } else if (newHole == 7){
+      } else if (newHole == 7) {
         mole.style.top = 444 + 'px';
         mole.style.left = 395 + 'px';
-      } else if (newHole == 8){
+      } else if (newHole == 8) {
         mole.style.top = 444 + 'px';
         mole.style.left = 667 + 'px';
-      } else if (newHole == 9){
+      } else if (newHole == 9) {
         mole.style.top = 444 + 'px';
         mole.style.left = 943 + 'px';
       }
 
-        // mole.style.top = 0 + 'px';
-        // mole.style.top = 222 + 'px';
-        // mole.style.top = 444 + 'px';
-        
-        // mole.style.left = 395 + 'px'; 
-        // mole.style.left = 667 + 'px';
-        // mole.style.left = 943 + 'px';
+
+    }
+
+    function newLocation() {
+      moveMole(moleDiv)
+    }
+
+    const moleInterval = setInterval(newLocation, 1000);
+
+    
+    moleDiv.addEventListener("click", addDead)
+
+    function addDead() {
+      moleDiv.classList.add('dead');
+      const moleOut = setTimeout(moleDie, 1000);
+      clearInterval(moleInterval);
+      
+
+      function moleDie() {
+        moleDiv.remove()
       }
+
+    }
     
-      function newLocation() {
-        moveMole(moleDiv) 
-      }
-    
-      setInterval(newLocation, 1000);
-    
-      return moleDiv
-}
-
-
-
-
-
-  
-  for (let i = 0; i < 5; i++) {
-    
-   
+    return moleDiv
   }
 
 
-  createMole()
+
+
+
+
+  for (let i = 0; i < 5; i++) {
+
+
+  }
+
+
+  // createMole()
 
 
 
