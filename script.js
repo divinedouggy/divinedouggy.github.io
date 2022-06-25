34c93c96c5ae3e63e2f62ac90d036cdd0eb43c5f
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-06-24 22:08:24 -0400

    have transition to level 2 - but level 2 loops as of now

--- script.js
+++ script.js
@@ -3,100 +3,7 @@ window.onload = function () {
 
   console.log(body);
 
-
-
-  const gameTitle = document.querySelector('h1');
-
-  titleGlow()
-
-  function titleGlow() {
-    gameTitle.style.transform = 'scale(1.02)';
-    gameTitle.classList.toggle('title')
-    setTimeout(titleUnglow, 1000)
-  }
-
-  function titleUnglow() {
-    gameTitle.style.transform = "";
-    gameTitle.classList.toggle('title')
-    setTimeout(titleGlow, 1000)
-  }
-
-  const startButton = document.querySelector('button');
-  const startScreen = document.querySelector('#startbox');
-
-  startButton.addEventListener("click", startGame);
-
-  function startGame() {
-    startButton.removeEventListener("click", startGame);
-    startButton.style.color = 'yellow';
-    // document.body.style.backgroundImage = 'url(images/background.png)';
-    startScreen.style.opacity = '0%';
-    startScreen.style.transform = 'scale(5)';
-    startButton.style.transform = 'scale(.5)';
-    const moleHoles = document.createElement('div');
-    moleHoles.setAttribute('id','moleholes');
-    document.body.appendChild(moleHoles)
-    const block = document.createElement('div');
-    block.className ='block';
-    document.body.appendChild(block);
-    const block2 = document.createElement('div');
-    block2.className ='block2';
-    document.body.appendChild(block2);
-    const block3 = document.createElement('div');
-    block3.className ='block3';
-    document.body.appendChild(block3);
-
-    const backGround = document.createElement('div');
-    backGround.className = 'grass';
-    document.body.appendChild(backGround);
-    
-    
-
-    
-
-    setTimeout(() => startScreen.remove(), 1000)
-
-    setTimeout(levelOne, 1000)
-  }
-
-  let levelNum = document.querySelector('h2');
-  let timer = document.querySelector('h3');
-
-
-
-  function levelOne() {
-
-    levelNum.style.display = '';
-    levelNum.style.transform = 'scale(3)';
-    levelNum.style.opacity = '100%';
-    setTimeout(() => levelNum.remove(), 2000);
-
-
-
-
-    setTimeout(setTimer, 2000,)
-    setTimeout(createMole, 3000)
-    setTimeout(createMole, 6000)
-  }
-
-  let time = '31'
-  function setTimer() {
-
-    let interval = setInterval(function () {
-      time--;
-      timer.innerText = `${time}`
-      if (time === 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
-  }
-
-
-
-
-
-
-
+  //// All the Mole Creation stuff
 
   function createMole() {
 
@@ -104,7 +11,7 @@ window.onload = function () {
     let moleDiv = document.createElement('div');
     moleDiv.className = 'mole';
     document.body.appendChild(moleDiv);
-    
+
 
     //Picks a random number (hole) between 1 and 9
     const randomHole = () => {
@@ -221,53 +128,167 @@ window.onload = function () {
       setTimeout(molePop, 1400)
     }
 
-    // titleGlow()
-
-    // function titleGlow() {
-    //   gameTitle.style.transform = 'scale(1.02)';
-    //   gameTitle.classList.toggle('title')
-    //   setTimeout(titleUnglow, 1000)
-    // }
-  
-    // function titleUnglow() {
-    //   gameTitle.style.transform = "";
-    //   gameTitle.classList.toggle('title')
-    //   setTimeout(titleGlow, 1000)
-    // }
 
     moleDiv.addEventListener("click", addDead)
 
+
     function addDead() {
+      splat.play();
+      moleDiv.removeEventListener("click", addDead);
+
+
       moleDiv.classList.add('dead');
-      moleDiv.style.zIndex = '9';
       clearInterval(moleInterval);
       clearTimeout(molePop);
       clearTimeout(moleunPop);
       const moleOut = setTimeout(moleDie, 2000);
 
-      
-
-
       function moleDie() {
         moleDiv.remove()
       }
+
+      setTimeout(checkForWinner, 2000);
 
     }
 
     return moleDiv
   }
 
+  //// Timer stuff
+  
+  let timer = document.querySelector('h3');
+  let time = '31'
+  function setTimer() {
+
+    let interval = setInterval(function () {
+      time--;
+      timer.innerText = `${time}`
+      if (time === 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  //// Title Screen Stuff
+
+  const gameTitle = document.querySelector('h1');
+
+  titleGlow()
+
+  function titleGlow() {
+    gameTitle.style.transform = 'scale(1.02)';
+    gameTitle.classList.toggle('title')
+    setTimeout(titleUnglow, 1000)
+  }
+
+  function titleUnglow() {
+    gameTitle.style.transform = "";
+    gameTitle.classList.toggle('title')
+    setTimeout(titleGlow, 1000)
+  }
+
+
+  const startButton = document.querySelector('button');
+  const startScreen = document.querySelector('#startbox');
+
+
+  //// Level slide
+  let levelNum = document.querySelector('h2');
+
+  //// Mole splat sound
+  let splat = new Audio('audio/splat.mp3');
+
+  
+  //// startButton is clicked, startGame
+
+  startButton.addEventListener("click", startGame);
+
+  function startGame() {
+    startButton.removeEventListener("click", startGame);
+    startButton.style.color = 'yellow';
+    startScreen.style.opacity = '0%';
+    startScreen.style.transform = 'scale(5)';
+    startButton.style.transform = 'scale(.5)';
+    const moleHoles = document.createElement('div');
+    moleHoles.setAttribute('id', 'moleholes');
+    document.body.appendChild(moleHoles)
+    const block = document.createElement('div');
+    block.className = 'block';
+    document.body.appendChild(block);
+    const block2 = document.createElement('div');
+    block2.className = 'block2';
+    document.body.appendChild(block2);
+    const block3 = document.createElement('div');
+    block3.className = 'block3';
+    document.body.appendChild(block3);
+
+    const backGround = document.createElement('div');
+    backGround.className = 'grass';
+    document.body.appendChild(backGround);
 
 
 
 
 
-  for (let i = 0; i < 5; i++) {
+    setTimeout(() => startScreen.remove(), 1000)
 
-
+    setTimeout(playGame, 1000)
   }
 
 
 
+
+
+
+
+  function playGame() {
+
+    //// Level One
+
+    levelOne()
+
+    function levelOne() {
+
+      // levelNum.style.display = '';
+      levelNum.style.transform = 'scale(5)';
+      levelNum.style.opacity = '100%';
+      setTimeout(() => levelNum.style.opacity='0%', 2000);
+
+      for (let i = 0; i < 5; i++) {
+    
+        setTimeout(createMole, 3000)
+        
+      }
+    
+
+
+      setTimeout(setTimer, 2000,)
+      
+
+      // setTimeout(createMole, 3000)
+      // setTimeout(createMole, 6000)
+    }
+  }
+
+
+  function checkForWinner() {
+    let moles = document.getElementsByClassName('mole').length;
+    if (moles == 0) {
+      timer.style.opacity = '0%'
+      levelTwo()
+    }
+  }
+
+  function levelTwo () {
+    levelNum.innerText = 'Level 2'
+    levelNum.style.opacity='100%'
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 5; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
 
 }
