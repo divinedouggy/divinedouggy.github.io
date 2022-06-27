7a38fb3dab559e4ceeb9a768e4a7742b43ef0057
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-06-27 00:50:46 -0400

    lots of updates, final push before submit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ window.onload = function () {
   const body = document.body;
 
   console.log(body);
+  let level = '';
 
   //// All the Mole Creation stuff
+  
 
   function createMole() {
 
@@ -130,10 +132,42 @@ window.onload = function () {
 
 
     moleDiv.addEventListener("click", addDead)
+    
+    
 
 
     function addDead() {
+      splat.load();
       splat.play();
+      hammer.load();
+      hammer.play();
+      const randomSound = () => {
+        return Math.floor(Math.random() * 20 + 1)
+      }
+      let newSound = randomSound();
+
+      if (newSound == 1) {
+        lol.play();
+      } else if (newSound == 2) {
+        pain.play();
+      } else if (newSound == 3) {
+        wee.play();
+      } else if (newSound == 4) {
+        ow.play();
+      } else if (newSound == 5) {
+        ah.play();
+      } else if (newSound == 6) {
+        pain.play();
+      } else if (newSound == 7) {
+        ah.play();
+      } else if (newSound == 8) {
+        ow.play();
+      } else if (newSound == 9) {
+        ah.play();
+      } else if (newSound == 10) {
+        ow.play();
+      } 
+      
       moleDiv.removeEventListener("click", addDead);
 
 
@@ -157,17 +191,57 @@ window.onload = function () {
   //// Timer stuff
   
   let timer = document.querySelector('h3');
-  let time = '31'
+  let time = '21'
+
   function setTimer() {
 
     let interval = setInterval(function () {
       time--;
       timer.innerText = `${time}`
+      if (time === 5) {
+        timer.style.color = 'red';
+      }
       if (time === 0) {
+        
         clearInterval(interval);
+        gameOver();
+
+
+        
+        
       }
     }, 1000);
   }
+
+  let coverLayer 
+
+  function gameOver() {
+    starMan.pause();
+    gameOverSound.play();
+    levelNum.style.opacity = '90%';
+    levelNum.innerText = 'GAME OVER'
+    levelNum.zIndex = '10';
+    levelNum.style.fontSize = '40px';
+    levelNum.style.fontFamily = 'Impact, fantasy';
+    levelNum.style.fontWeight = 'lighter'
+    levelNum.style.transform = 'scale(8)';
+    levelNum.style.pointerEvents = 'initial';
+    levelNum.style.background = 'none';
+    levelNum.style.border = 'none';
+    levelNum.style.transition = '.5s'
+    levelNum.style.color = 'red'
+
+  
+  
+    timer.style.opacity = '0%';
+
+    
+    
+  }
+
+  
+
+  
 
   //// Title Screen Stuff
 
@@ -197,7 +271,18 @@ window.onload = function () {
 
   //// Mole splat sound
   let splat = new Audio('audio/splat.mp3');
+  let starMan = new Audio('audio/mario.mp3');
+  let lol = new Audio('audio/lol.mp3');
+  let pain = new Audio('audio/pain.mp3');
+  let wee = new Audio('audio/wee.mp3');
+  let ow = new Audio('audio/ow.mp3');
+  let ah = new Audio('audio/ah.mp3');
+  let hammer = new Audio('audio/hammer.mp3');
+  let gameOverSound = new Audio('audio/gameover.mp3')
+  let clear = new Audio('audio/clear.mp3')
 
+  const backGround = document.createElement('div');
+  backGround.className = 'grass';
   
   //// startButton is clicked, startGame
 
@@ -205,10 +290,13 @@ window.onload = function () {
 
   function startGame() {
     startButton.removeEventListener("click", startGame);
-    startButton.style.color = 'yellow';
+    starMan.load();
+    starMan.play();
+    starMan.loop = true;
+    // startButton.style.color = 'yellow';
     startScreen.style.opacity = '0%';
     startScreen.style.transform = 'scale(5)';
-    startButton.style.transform = 'scale(.5)';
+    // startButton.style.transform = 'scale(.5)';
     const moleHoles = document.createElement('div');
     moleHoles.setAttribute('id', 'moleholes');
     document.body.appendChild(moleHoles)
@@ -222,8 +310,7 @@ window.onload = function () {
     block3.className = 'block3';
     document.body.appendChild(block3);
 
-    const backGround = document.createElement('div');
-    backGround.className = 'grass';
+
     document.body.appendChild(backGround);
 
 
@@ -235,10 +322,30 @@ window.onload = function () {
     setTimeout(playGame, 1000)
   }
 
-
-
-
-
+  function checkForWinner() {
+    let moles = document.getElementsByClassName('mole').length;
+    if (moles == 0) {
+      timer.style.opacity = '0%';
+      if (level == 1) {
+        levelTwo()
+      } else if (level == 2) {
+        levelThree()
+      } else if (level == 3) {
+        levelFour()
+      } else if (level == 4) {
+        levelFive()
+      } else if (level == 5) {
+        levelSix()
+      } else if (level == 6) {
+        levelSeven()
+      } else if (level == 7) {
+        levelEight()
+      } else if (level == 8) {
+        gameComplete()
+      }
+      
+    }
+  }
 
 
   function playGame() {
@@ -248,13 +355,13 @@ window.onload = function () {
     levelOne()
 
     function levelOne() {
-
+      level = 1;
       // levelNum.style.display = '';
       levelNum.style.transform = 'scale(5)';
       levelNum.style.opacity = '100%';
       setTimeout(() => levelNum.style.opacity='0%', 2000);
 
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 3; i++) {
     
         setTimeout(createMole, 3000)
         
@@ -263,6 +370,7 @@ window.onload = function () {
 
 
       setTimeout(setTimer, 2000,)
+     
       
 
       // setTimeout(createMole, 3000)
@@ -271,21 +379,180 @@ window.onload = function () {
   }
 
 
-  function checkForWinner() {
-    let moles = document.getElementsByClassName('mole').length;
-    if (moles == 0) {
-      timer.style.opacity = '0%'
-      levelTwo()
-    }
-  }
 
-  function levelTwo () {
+
+  function levelTwo() {
+    level = 2;
     levelNum.innerText = 'Level 2'
     levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'DarkKhaki';
+    backGround.style.backgroundImage = "url('images/backgrounded2.png')"
+
     setTimeout(() => levelNum.style.opacity='0%', 2000);
     for (let i = 0; i < 5; i++) {
     
       setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function levelThree() {
+    level = 3;
+    levelNum.innerText = 'Level 3'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'lightblue';
+    backGround.style.backgroundImage = "url('images/backgrounded3.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 7; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function levelFour() {
+    level = 4;
+    levelNum.innerText = 'Level 4'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'DarkGreen';
+    backGround.style.backgroundImage = "url('images/backgrounded4.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 9; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function levelFive() {
+    level = 5;
+    levelNum.innerText = 'Level 5'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'MediumPurple';
+    backGround.style.backgroundImage = "url('images/backgrounded5.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 11; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+  
+  function levelSix() {
+    level = 6;
+    levelNum.innerText = 'Level 6'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'MidnightBlue';
+    backGround.style.backgroundImage = "url('images/backgrounded6.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 13; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function levelSeven() {
+    level = 7;
+    levelNum.innerText = 'Level 7'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'SaddleBrown';
+    backGround.style.backgroundImage = "url('images/backgrounded7.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 15; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function levelEight () {
+    level = 8;
+    levelNum.innerText = 'Level 8'
+    levelNum.style.opacity='100%'
+
+    setTimeout(() => time = '21', 2000);
+    setTimeout(() => timer.style.color = 'black', 2000);
+    setTimeout(() => timer.style.opacity = '100%', 3000);
+    
+    document.body.style.backgroundColor = 'DarkGray';
+    backGround.style.backgroundImage = "url('images/backgrounded8.png')"
+
+    setTimeout(() => levelNum.style.opacity='0%', 2000);
+    for (let i = 0; i < 17; i++) {
+    
+      setTimeout(createMole, 3000)
+      
+    }
+
+  }
+
+  function gameComplete(){
+    starMan.pause();
+    gameOverSound.remove();
+    clear.play();
+
+    levelNum.style.opacity = '100%';
+    levelNum.innerText = 'YOU WIN!'
+    levelNum.zIndex = '10';
+    levelNum.style.fontSize = '40px';
+    levelNum.style.fontFamily = 'Impact, fantasy';
+    levelNum.style.transform = 'scale(8)';
+    backGround.style.backgroundImage = "url('images/backgrounded9.png')"
+    setTimeout(congratsText, 4000)
+    setTimeout(() => levelNum.remove(), 3000);
+    let killMoleHoles = document.getElementById('moleholes');
+    killMoleHoles.remove();
+   
+    time = ''
+    
+    
+
+    function congratsText() {
+      
+      let congrats = document.querySelector('h4');
+      congrats.innerText = 'Congratulations!'
       
     }
 
